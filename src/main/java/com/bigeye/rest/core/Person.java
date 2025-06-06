022f485cb94ba3c67b43d660eb3e4da7c166ae0d
Jeff Cole <jeff@bigeye.com>
2025-06-06 15:19:21 -0700

    Initial commit

---
+++ src/main/java/com/bigeye/rest/core/Person.java
@@ -0,0 +1,67 @@
+package com.bigeye.rest.core;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "job_title", nullable = false)
+    private String jobTitle;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "person")
+    @JsonManagedReference
+    private List<Login> logins;
+
+    public Person() {
+        this.logins = Collections.emptyList();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public List<Login> getLogins() {
+        return logins;
+    }
+
+    public void setLogins(List<Login> logins) {
+        this.logins = logins;
+    }
+}
